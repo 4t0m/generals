985886b8b695fef49a5b70fd19b36093d4925559
Adom Hartell <adomhartell@gmail.com>
2017-03-28 11:06:12 -0700

    setup basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './lib/board.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*']
+  }
+};
